11d2158e5a38ba54c546e23926d031c0ea9b7f6a
Diogo Rodrigues <diogorodrigues699@yahoo.com>
2024-02-23 14:54:39 -0300

    ecommerce

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+	echo "OK";
+
+});
+
+$app->run();
+
+
+
+echo "DIOGO TA NA CASA PORRAAAAAA"
+
+ ?>
